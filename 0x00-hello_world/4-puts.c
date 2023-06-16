30a16aea073ef56551cafeb4d9307da11f98479c
Salmanebm <salmanesbm@gmail.com>
2023-06-16 12:34:13 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-void main(void) {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
