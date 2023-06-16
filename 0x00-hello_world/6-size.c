f6a8163f6593270c3c9a34b90c3b4e9b7c5ea8b0
Salmanebm <salmanesbm@gmail.com>
2023-06-16 13:43:29 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,6 @@ int main(void)
 	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of float: %lu byte(s)\n", sizeof(float));
-	printf("Size of double: %lu byte(s)\n", sizeof(double));
 
 	return 0;
 }
