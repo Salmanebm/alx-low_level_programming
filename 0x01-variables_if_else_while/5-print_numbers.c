5b74417f02a6c7f5e4856552b68abe87b9e0ff53
Salmanebm <salmanesbm@gmail.com>
2023-06-17 07:39:37 -0700

    single

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
- *              Uses the putchar function.
+ * Description: Write a program that prints all single digit numbers 
+ *         of base 10 starting from 0, followed by a new line
  *
  * Return: Always 0 (Success)
  */
@@ -12,7 +12,9 @@ int main(void)
 {
 int n;
 for (n = 0 ; n <= 9 ; n++)
-{ if (n % 10 != 0)
-printf("%d\n",n);
-} return (0);
+{
+if (n % 10 != 0)
+printf("%d", n);
+} printf("\n");
+return (0);
 }
