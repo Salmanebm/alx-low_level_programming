11ead421869edea8b522dbb2dfc6e3bce15ab037
Salmanebm <salmanesbm@gmail.com>
2023-06-17 05:05:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,17 +5,17 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints the message "and that piece of art is useful" - Dora Korpar, 2015-10-19"
- *              to the standard error.
+ * Description: Prints the message "and that piece of art is useful"
+ *              - Dora Korpar, 2015-10-19" to the standard error.
  *
  * Return: Always 1
  */
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    size_t len = strlen(message);
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t len = strlen(message);
 
-    syscall(SYS_write, STDERR_FILENO, message, len);
+	syscall(SYS_write, STDERR_FILENO, message, len);
 
-    return 1;
+	return (1);
 }
