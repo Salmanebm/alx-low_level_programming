d01644a48c56bd0193cc6330ed9e4d8e9ea23893
Salmanebm <salmanesbm@gmail.com>
2023-06-17 05:08:57 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,10 +12,11 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *message = "and that piece of art is useful\" - "
+		"Dora Korpar, 2015-10-19\n";
 	size_t len = strlen(message);
 
 	syscall(SYS_write, STDERR_FILENO, message, len);
 
-	return (1);
+	return 1;
 }
