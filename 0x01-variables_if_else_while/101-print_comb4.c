ce2dff72385def440b87da98601adf2c2b646ab4
Salmanebm <salmanesbm@gmail.com>
2023-06-18 14:13:14 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ *  program prints all possible different combinations
+ *  of three digits, separated by ", " and in ascending order.
+ *  adheres to the given restrictions.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit1, digit2, digit3;
+
+for (digit1 = 0; digit1 < 8; digit1++)
+{
+for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+{
+for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+{
+putchar('0' + digit1);
+putchar('0' + digit2);
+putchar('0' + digit3);
+
+if (digit1 != 7 || digit2 != 8 || digit3 != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+}
