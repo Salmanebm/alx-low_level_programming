d5c5b24ddb3b8f0f8aa313d1cc1fd802c982d024
Salmanebm <salmanesbm@gmail.com>
2023-06-18 13:07:28 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
- * Description: Prints the numbers of base 16, followed by a new line.
- *              Uses the putchar function.
+ * Description: This program prints all the numbers of base 16 in lowercase,
+ *              followed by a new line.
+ *              It uses the putchar function and follows the Betty coding style.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n;
-char letter;
-for (letter = '0'; n <= '9'; n++)
-{
-putchar(letter);
-}
-for (letter = 'a'; letter <= 'f'; n++)
-{
-putchar(letter);
-}
-putchar('\n');
+int digit;
 
+for (digit = 0; digit < 10; digit++)
+{
+putchar('0' + digit);
+}
+
+for (digit = 0; digit < 6; digit++)
+{
+putchar('a' + digit);
+}
+
+putchar('\n');
 return (0);
 }
