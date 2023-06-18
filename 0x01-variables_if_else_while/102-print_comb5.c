af01b43bdfa90372245261dd692b40509b52d4b8
Salmanebm <salmanesbm@gmail.com>
2023-06-18 14:51:08 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point of the program
  *
- *  prints all possible combinations of two two-digit numbers
+ * prints all possible combinations of two two-digit numbers
  *  
  * Return: Always 0 (Success)
  */
