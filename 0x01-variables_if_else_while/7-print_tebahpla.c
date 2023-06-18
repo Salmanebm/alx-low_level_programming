811751c6e4a45a69c6181394aa7f1697bcccdc98
Salmanebm <salmanesbm@gmail.com>
2023-06-18 12:41:18 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercas in reve, followed by a new line.
+ *              Uses the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter;
+
+for (letter = 'z'; letter <= 'a'; letter--)
+{
+putchar(letter);
+}
+
+putchar('\n');
+
+return (0);
+}
