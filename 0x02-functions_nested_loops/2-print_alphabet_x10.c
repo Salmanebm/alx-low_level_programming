ae1622e1635d469eed9d0c3f405d1d7dd1af5da2
Salmanebm <salmanesbm@gmail.com>
2023-06-21 14:53:13 -0700

    10 times alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,11 +2,9 @@
 
 
 	/**
-	 * Description: print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+	 * Description: print_alphabet_x10 - prints 10 times the alphabet,
 	 * followed by a new line
 	 */
-
-
 void print_alphabet_x10(void)
 {
 char l;
