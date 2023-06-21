c1d33e34bcca5f8f8e3b1e99b13ea552727893df
Salmanebm <salmanesbm@gmail.com>
2023-06-21 14:57:09 -0700

    10 times alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,12 @@
 #include "main.h"
-
-
-	/**
-	 * Description: print_alphabet_x10 - prints 10 times the alphabet,
-	 * followed by a new line
-	 */
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the alphabet 10 times.
+ * Each letter is printed using the putchar function.
+ *
+ * Return: 0 (Success)
+ */
 void print_alphabet_x10(void)
 {
 char l;
