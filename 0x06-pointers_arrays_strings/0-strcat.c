876bbfd174e3d79028bd285dc32c4ff9fe5f7a54
Salmanebm <salmanesbm@gmail.com>
2023-07-01 08:20:02 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * @*_strcat - concatenate two strings
+ *
+ * Return: string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest,src));
+}
