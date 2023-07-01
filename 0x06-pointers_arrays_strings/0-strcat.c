c4b3aad847fd4d9bd84b4d9bac34c295a0cf9bd7
Salmanebm <salmanesbm@gmail.com>
2023-07-01 08:29:44 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * @_strcat: - concatenate two strings
- * @dest,@src: two string to concat
+ * strcat: - concatenate two strings
+ * @dest, @src: two string to concat
  *
- * Return: string.
+ * Return: string concatenated.
  */
 char *_strcat(char *dest, char *src)
 {
