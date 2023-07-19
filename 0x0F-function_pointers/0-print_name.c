a355393d1fa34d5e9c0ab93ff0096fea22aad061
Salmanebm <salmanesbm@gmail.com>
2023-07-19 05:30:55 -0700

    name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,9 +6,10 @@
  * @f: pointer to function
  * Return: nothing
  **/
-
-
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
