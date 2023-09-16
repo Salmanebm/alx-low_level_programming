f2b2296792f172c923551f7426c244fb7964292c
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-09-16 06:11:57 -0700

    len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * dlistint_len - count element
+ * @h: list to print
+ * Return:number of nodes.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
